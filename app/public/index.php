dd37edbf73cd86c739b24c3b5f2675c5440aa36f
sajonaro <alex.kunich@gmail.com>
2024-05-05 00:30:26 +0300

    configured Hello world page

---
+++ app/public/index.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+    <title>My First PHP Page</title>
+</head>
+<body>
+    <h1>session testing PHP Page</h1>
+    <?php
+        echo "Hello World!";
+    ?>          
+</body>
+
+</html>
